a4314d69e97b09a2f7bd3ba1bda83fa93f74a9cd
FLora <foamfly@gmail.com>
2017-08-29 09:05:12 -0400

    add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// import { DashboardComponent }   from './dashboard.component';
+import { LoginComponent }      from './login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // { path: 'dashboard',  component: DashboardComponent },
+  { path: 'login',     component: LoginComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
